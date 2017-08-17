4cedb7df6f326b11713717f878cae239e00f1287
Nathan Drake <pet@pet-drake.inf.ufrgs.br>
2017-08-17 17:41:51 -0300

    Organized files in folders. Updated button styling

--- login.js
+++
@@ -1,6 +0,0 @@
-// make a POST request with login information for the server
-document.getElementById('login-button').addEventListener('click', function(){
-	console.log('loggin user info:');
-	console.log(document.getElementById('user-email').value);
-	console.log(document.getElementById('user-password').value);
-});
