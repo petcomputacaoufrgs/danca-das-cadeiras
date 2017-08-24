12153ed45ace3d6a0f405b6352932afa59091e4b
Ed Sheraan <petcompufrgs@gmail.com>
2017-08-24 17:11:44 -0300

    routed existing pages (login and register) with express

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var __viewsdir = __dirname + "/views/";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// tells express to look for things in the public folder aswell
+// by default it will only look at the views folder
+app.use(express.static( __viewsdir + "assets/")); 
+
+// tells express to expect .ejs as default files on .render() and paths, unless otherwise specified;
+app.set("view engine", "ejs");
+
+
+app.get('/', function(req, res){
+	res.render("login");
+});
+
+app.get('/register', function(req, res){
+	res.render("register");
+});
+
+app.get("*", function(req, res){
+	// do some funny image
+	res.send("This page doesn't exists!!!!!11!!11");
+});
+
+app.listen(5000, 'localhost', function() {
+	console.log("Server Online");
+});
