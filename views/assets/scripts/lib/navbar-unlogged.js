54d74ac0bdda55a34f2bb15e15f69ffe2614bcfc
Pedro Cardouzo <pedro.emilio.zardo@hotmail.com>
2017-08-27 03:50:58 -0300

    More styling changes. Decided that the login post route will be '/login'

--- views/assets/scripts/lib/navbar-unlogged.js
+++ views/assets/scripts/lib/navbar-unlogged.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.dropdown-toggle').on('click', function(){
-    	$("div.dropdown-form").toggleClass("hide");
+   		$("div.dropdown-form").slideToggle(150);
+   		$("div.spacing").slideToggle(150);
     });
 });
