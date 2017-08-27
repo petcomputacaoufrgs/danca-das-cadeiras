405f383ae9938e2dfd8399f51f1bc6779a2f1f8e
Pedro Cardouzo <pedro.emilio.zardo@hotmail.com>
2017-08-27 16:49:19 -0300

    added post route for the '/' and '/register' page

--- app.js
+++ app.js
@@ -18,8 +18,35 @@ app.get('/', function(req, res){
 	res.render("login");
 });
 
+app.post('/login', function(req, res){
+	var email = req.body.email; 
+	var password = req.body.password;
+	var rememberMe = req.body.rememberMe; // "on" if box was checked, undefined otherwise
+
+	console.log("Email: " + email); // of course this is not staying
+	console.log("Password: " + password); // of course this is not staying
+	console.log("RM: " + rememberMe); // of course this is not staying
+	res.send("heyyy");
+});
+
 app.get('/register', function(req, res){
 	res.render("register");
+});
+
+app.post('/register', function(req, res){
+	var name = req.body.username,
+		email = req.body.email,
+		password = req.body.password,
+		course = req.body.course,
+		internship = req.body.internship;
+	
+	console.log("name: " + name);
+	console.log("email: " + email);
+	console.log("password: " + password);
+	console.log("course: " + course);
+	console.log("internship: " + internship);	
+
+	res.redirect("/");
 });
 
 app.get("*", function(req, res){
